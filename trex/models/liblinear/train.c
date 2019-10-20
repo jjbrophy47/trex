e5f3969a49768090b8264fa1aa9850a5f5cd749c
jbrophy <jonathanbrophy47@gmail.com>
2019-10-20 13:31:43 -0700

    fixing liblinear

--- trex/models/liblinear/train.c
+++ trex/models/liblinear/train.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <errno.h>
-#include <Python.h>
+// #include <Python.h>
 #include "linear.h"
 #define Malloc(type,n) (type *)malloc((n)*sizeof(type))
 #define INF HUGE_VAL
@@ -100,35 +100,35 @@ int flag_solver_specified;
 int nr_fold;
 double bias;
 
-// Function 1: A simple 'hello world' function
-static PyObject* helloworld(PyObject* self, PyObject* args)
-{
-    printf("Hello World\n");
-    return Py_None;
-}
+// // Function 1: A simple 'hello world' function
+// static PyObject* helloworld(PyObject* self, PyObject* args)
+// {
+//     printf("Hello World\n");
+//     return Py_None;
+// }
 
-// Our Module's Function Definition struct
-// We require this `NULL` to signal the end of our method
-// definition
-static PyMethodDef myMethods[] = {
-    { "helloworld", helloworld, METH_NOARGS, "Prints Hello World" },
-    { NULL, NULL, 0, NULL }
-};
+// // Our Module's Function Definition struct
+// // We require this `NULL` to signal the end of our method
+// // definition
+// static PyMethodDef myMethods[] = {
+//     { "helloworld", helloworld, METH_NOARGS, "Prints Hello World" },
+//     { NULL, NULL, 0, NULL }
+// };
 
-// Our Module Definition struct
-static struct PyModuleDef myModule = {
-    PyModuleDef_HEAD_INIT,
-    "myModule",
-    "Test Module",
-    -1,
-    myMethods
-};
+// // Our Module Definition struct
+// static struct PyModuleDef myModule = {
+//     PyModuleDef_HEAD_INIT,
+//     "myModule",
+//     "Test Module",
+//     -1,
+//     myMethods
+// };
 
-// Initializes our module using our above struct
-PyMODINIT_FUNC PyInit_myModule(void)
-{
-    return PyModule_Create(&myModule);
-}
+// // Initializes our module using our above struct
+// PyMODINIT_FUNC PyInit_myModule(void)
+// {
+//     return PyModule_Create(&myModule);
+// }
 
 int main(int argc, char **argv)
 {
