5a50a1e6d370668fbb1d0527c9c8d8cf09d045fd
jbrophy <jonathanbrophy47@gmail.com>
2020-05-19 21:38:02 -0700

    batch change

--- jobs/performance/batch.sh
+++ jobs/performance/batch.sh
@@ -1,8 +1,19 @@
-sbatch jobs/performance/template.sh churn cb
+sbatch jobs/performance/template.sh churn svm_linear
 sbatch jobs/performance/template.sh churn svm_rbf
-sbatch jobs/performance/template.sh amazon cb
+sbatch jobs/performance/template.sh churn lr
+sbatch jobs/performance/template.sh churn knn
+
+sbatch jobs/performance/template.sh amazon svm_linear
 sbatch jobs/performance/template.sh amazon svm_rbf
-sbatch jobs/performance/template.sh adult cb
+sbatch jobs/performance/template.sh amazon lr
+sbatch jobs/performance/template.sh amazon knn
+
+sbatch jobs/performance/template.sh adult svm_linear
 sbatch jobs/performance/template.sh adult svm_rbf
-sbatch jobs/performance/template.sh census cb
+sbatch jobs/performance/template.sh adult lr
+sbatch jobs/performance/template.sh adult knn
+
+sbatch jobs/performance/template.sh census svm_linear
 sbatch jobs/performance/template.sh census svm_rbf
+sbatch jobs/performance/template.sh census lr
+sbatch jobs/performance/template.sh census knn
