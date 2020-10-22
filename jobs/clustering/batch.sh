6407a63dda2cc5166157232be4fe2096232232b8
jbrophy <jonathanbrophy47@gmail.com>
2020-10-22 12:06:38 -0700

    fixed batch script

--- jobs/clustering/batch.sh
+++ jobs/clustering/batch.sh
@@ -1,5 +1,5 @@
 # CB
-./jobs/cleaning/primer.sh churn cb 100 3 6 1440 short
-./jobs/cleaning/primer.sh amazon cb 250 5 9 1440 short
-./jobs/cleaning/primer.sh adult cb 100 5 9 1440 short
-./jobs/cleaning/primer.sh census cb 250 5 30 1440 short
+./jobs/clustering/primer.sh churn cb 100 3 6 1440 short
+./jobs/clustering/primer.sh amazon cb 250 5 9 1440 short
+./jobs/clustering/primer.sh adult cb 100 5 9 1440 short
+./jobs/clustering/primer.sh census cb 250 5 30 1440 short
