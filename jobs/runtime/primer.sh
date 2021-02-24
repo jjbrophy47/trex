3de22ce11881ffef9533ccc2f58f2534003afe5f
jbrophy <jonathanbrophy47@gmail.com>
2021-02-23 21:34:14 -0800

    small update

--- jobs/runtime/primer.sh
+++ jobs/runtime/primer.sh
@@ -10,8 +10,8 @@ rs_list=(1 2 3 4 5)
 method_list=('klr-leaf_output' 'svm-leaf_output',
              'leaf_influence' 'maple' 'knn-leaf_output')
 
-for rs in ${rs_list[@]}; do
-    for method in ${method_list[@]}; do
+for method in ${method_list[@]}; do
+    for rs in ${rs_list[@]}; do
         job_name="RT_${dataset}_${method}"
         sbatch --mem=${mem}G \
                --time=$time \
