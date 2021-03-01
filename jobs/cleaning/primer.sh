0294a6741c5d5afd51d38c97b8b85130f686e3a8
jbrophy <jonathanbrophy47@gmail.com>
2021-02-28 17:25:34 -0800

    updating cleaning scripts

--- jobs/cleaning/primer.sh
+++ jobs/cleaning/primer.sh
@@ -1,12 +1,13 @@
 dataset=$1
 model=$2
-n_estimators=$3
-max_depth=$4
-check_pct=$5
-train_frac=$6
-mem=$7
-time=$8
-partition=$9
+preprocessing=$3
+n_estimators=$4
+max_depth=$5
+check_pct=$6
+train_frac=$7
+mem=$8
+time=$9
+partition=${10}
 
 rs_list=(1 2 3 4 5)
 method_list=('random' \
@@ -25,7 +26,7 @@ for method in ${method_list[@]}; do
                --job-name=$job_name \
                --output=jobs/logs/cleaning/$job_name \
                --error=jobs/errors/cleaning/$job_name \
-               jobs/cleaning/runner.sh $dataset $model $n_estimators \
-               $max_depth $method $check_pct $train_frac $rs
+               jobs/cleaning/runner.sh $dataset $model $preprocessing \
+               $n_estimators $max_depth $method $check_pct $train_frac $rs
     done
 done
--- jobs/cleaning/runner.sh
+++ jobs/cleaning/runner.sh
@@ -6,16 +6,18 @@ module load python3/3.7.5
 
 dataset=$1
 model=$2
-n_estimators=$3
-max_depth=$4
-method=$5
-check_pct=$6
-train_frac=$7
-rs=$8
+preprocessing=$3
+n_estimators=$4
+max_depth=$5
+method=$6
+check_pct=$7
+train_frac=$8
+rs=$9
 
 python3 scripts/experiments/cleaning.py \
   --dataset $dataset \
   --model $model \
+  --preprocessing $preprocessing \
   --n_estimators $n_estimators \
   --max_depth $max_depth \
   --method $method \
