65f7455df16debaf2e1c3278f738b6f44c3a1b4f
jbrophy <jonathanbrophy47@gmail.com>
2021-03-08 17:51:25 -0800

    updating batch

--- jobs/roar/batch.sh
+++ jobs/roar/batch.sh
@@ -1,17 +1,17 @@
 # CatBoost: all methods, experiments are executed in chunks of 20 runs at a time
 ./jobs/roar/primer.sh 'churn' 'cb' 'standard' 100 3 'leaf_path' 0.01 'leaf_path' 61 2 1440 'short' 1
 ./jobs/roar/primer.sh 'surgical' 'cb' 'standard' 250 5 'tree_output' 1.0 'leaf_path' 15 2 1440 'short' 1
-./jobs/roar/primer.sh 'vaccine' 'cb' 'standard' 250 5 'tree_output' 1.0 'tree_output' 61 10 1440 'short' 1
+./jobs/roar/primer.sh 'vaccine' 'cb' 'standard' 250 5 'tree_output' 1.0 'tree_output' 61 2 1440 'short' 1
 ./jobs/roar/primer.sh 'amazon' 'cb' 'standard' 250 7 'tree_output' 1.0 'feature_path' 7 15 1440 'short' 1
-./jobs/roar/primer.sh 'bank_marketing' 'cb' 'standard' 250 5 'tree_output' 1.0 'tree_output' 31 10 1440 'short' 1
-./jobs/roar/primer.sh 'adult' 'cb' 'standard' 250 5 'tree_output' 1.0 'tree_output' 61 10 1440 'short' 1
-./jobs/roar/primer.sh 'census' 'cb' 'standard' 250 5 'tree_output' 1.0 'tree_output' 31 20 1440 'short' 1
+./jobs/roar/primer.sh 'bank_marketing' 'cb' 'standard' 250 5 'tree_output' 1.0 'tree_output' 31 2 1440 'short' 1
+./jobs/roar/primer.sh 'adult' 'cb' 'standard' 250 5 'tree_output' 1.0 'tree_output' 61 2 1440 'short' 1
+./jobs/roar/primer.sh 'census' 'cb' 'standard' 250 5 'tree_output' 1.0 'tree_output' 31 6 1440 'short' 1
 
 # RF: all methods, experiments are executed in chunks of 20 runs at a time
 ./jobs/roar/primer.sh 'churn' 'rf' 'standard' 100 3 'leaf_output' 0.01 'leaf_path' 61 2 1440 'short' 1
-./jobs/roar/primer.sh 'surgical' 'rf' 'standard' 250 5 'leaf_path' 0.001 'feature_output' 61 2 1440 'short' 1
-./jobs/roar/primer.sh 'vaccine' 'rf' 'standard' 250 5 'leaf_output' 0.001 'feature_output' 61 10 1440 'short' 1
-./jobs/roar/primer.sh 'amazon' 'rf' 'categorical' 250 7 'tree_output' 0.01 'tree_output' 61 15 1440 'short' 1
-./jobs/roar/primer.sh 'bank_marketing' 'rf' 'standard' 250 5 'leaf_output' 0.001 'leaf_path' 61 10 1440 'short' 1
-./jobs/roar/primer.sh 'adult' 'rf' 'standard' 250 5 'leaf_output' 0.001 'feature_output' 61 10 1440 'short' 1
-./jobs/roar/primer.sh 'census' 'rf' 'standard' 250 5 'tree_output' 0.001 'leaf_path' 61 20 1440 'short' 1
+./jobs/roar/primer.sh 'surgical' 'rf' 'standard' 250 5 'leaf_path' 0.001 'feature_output' 61 6 1440 'short' 1
+./jobs/roar/primer.sh 'vaccine' 'rf' 'standard' 250 5 'leaf_output' 0.001 'feature_output' 61 15 1440 'short' 1
+./jobs/roar/primer.sh 'amazon' 'rf' 'categorical' 250 7 'tree_output' 0.01 'tree_output' 61 2 1440 'short' 1
+./jobs/roar/primer.sh 'bank_marketing' 'rf' 'standard' 250 5 'leaf_output' 0.001 'leaf_path' 61 20 1440 'short' 1
+./jobs/roar/primer.sh 'adult' 'rf' 'standard' 250 5 'leaf_output' 0.001 'feature_output' 61 17 1440 'short' 1
+./jobs/roar/primer.sh 'census' 'rf' 'standard' 250 5 'tree_output' 0.001 'leaf_path' 61 6 1440 'short' 1
