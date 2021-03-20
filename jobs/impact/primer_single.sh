250c3cd980477ec96ef8147951b5e4c05669e925
jbrophy <jonathanbrophy47@gmail.com>
2021-03-20 09:29:48 -0700

    adjusting resource needs for fast_leaf_influence

--- jobs/impact/primer_single.sh
+++ jobs/impact/primer_single.sh
@@ -4,12 +4,15 @@ preprocessing=$3
 n_estimators=$4
 max_depth=$5
 method=$6
-mem=$7
-time=$8
-partition=$9
+start_pred=$7
+setting=$8
+$n_test=$9
+mem=${10}
+time=${11}
+partition=${12}
 
 for rs in {1..20}; do
-    job_name="I_${dataset}_${model}_${method}"
+    job_name="I_${dataset}_${model}_${method}_${setting}_${start_pred}_${n_test}"
 
     sbatch --mem=${mem}G \
            --time=$time \
@@ -18,6 +21,5 @@ for rs in {1..20}; do
            --output=jobs/logs/impact/$job_name \
            --error=jobs/errors/impact/$job_name \
            jobs/impact/runner.sh $dataset $model $preprocessing \
-           $n_estimators $max_depth $method \
-           'tree_output' 1.0 'tree_output' 61 $rs
+           $n_estimators $max_depth $method $start_pred $setting $n_test $rs
 done
