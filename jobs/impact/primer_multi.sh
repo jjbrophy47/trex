e11b2318c0eb085ec51a695edb25f2f3f508a330
jbrophy <jonathanbrophy47@gmail.com>
2021-04-13 22:38:00 -0700

    changing primer_multi

--- jobs/impact/primer_multi.sh
+++ jobs/impact/primer_multi.sh
@@ -8,9 +8,9 @@ time=$7
 partition=$8
 
 method_list=('klr_og_weighted_leaf_path_alpha' 'klr_og_weighted_leaf_path_sim' 'klr_og_weighted_leaf_path')
-             # 'klr_og_leaf_path_alpha' 'klr_og_leaf_path_sim' 'klr_og_leaf_path')
+             'klr_og_leaf_path_alpha' 'klr_og_leaf_path_sim' 'klr_og_leaf_path' 'random' 'bacon')
 setting_list=('dynamic')
-n_test_list=(100)
+n_test_list=(1000)
 start_pred_list=(-1)
 
 for setting in ${setting_list[@]}; do
