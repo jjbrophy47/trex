5c7c28ec1b40059282a5bee18bda1e240f49bd4e
jbrophy <jonathanbrophy47@gmail.com>
2021-04-23 14:06:28 -0700

    changed model values

--- jobs/impact_high_loss/batch.sh
+++ jobs/impact_high_loss/batch.sh
@@ -2,10 +2,11 @@
 
 # CB
 ./jobs/impact_high_loss/primer.sh 'churn'          'cb' 'standard' 100 3 2  300  'short'
-./jobs/impact_high_loss/primer.sh 'surgical'       'cb' 'standard' 250 5 2  300  'short'
-./jobs/impact_high_loss/primer.sh 'vaccine'        'cb' 'standard' 250 5 15 300  'short'
-./jobs/impact_high_loss/primer.sh 'bank_marketing' 'cb' 'standard' 250 5 15 300  'short'
-./jobs/impact_high_loss/primer.sh 'adult'          'cb' 'standard' 250 5 15 300  'short'
+./jobs/impact_high_loss/primer.sh 'surgical'       'cb' 'standard' 250 7 2  300  'short'
+./jobs/impact_high_loss/primer.sh 'vaccine'        'cb' 'standard' 250 7 15 300  'short'
+./jobs/impact_high_loss/primer.sh 'bank_marketing' 'cb' 'standard' 250 7 15 300  'short'
+./jobs/impact_high_loss/primer.sh 'adult'          'cb' 'standard' 250 7 15 300  'short'
+./jobs/impact_high_loss/primer.sh 'synthetic'      'cb' 'standard' 250 7 15 300  'short'
 
 # RF
 ./jobs/impact_high_loss/primer.sh 'churn'          'rf' 'standard'    100 3 5  300 'short'
